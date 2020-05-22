36d1d4f571018b8f81ad7d9bb697ff6d67d8f6ab
Shaun-Shane <1953181@tongji.edu.cn>
2020-05-22 19:34:33 +0800

    Update Actor Entity class

--- Classes/Actor/Entity.cpp
+++ Classes/Actor/Entity.cpp
@@ -1,1 +1,19 @@
 #include "Entity.h"
+
+Entity::Entity() : attack(3), armor(5), HP(5), MP(5) { this->sprite = nullptr; }
+
+Entity::Entity(Sprite* sprite) : attack(3), armor(5), HP(5), MP(5) {
+  this->sprite = sprite;
+  this->addChild(this->sprite);
+}
+
+Entity::~Entity() {}
+
+Sprite* Entity::GetSprite() { return this->sprite; }
+
+void Entity::BindSprite(Sprite* sprite) {
+  this->sprite = sprite;
+  this->addChild(this->sprite);
+}
+
+
--- Classes/Actor/Entity.h
+++ Classes/Actor/Entity.h
@@ -1,2 +1,26 @@
-#pragma once
-class Entity {};
+#ifndef _ENTITY_H_
+#define _ENTITY_H_
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class Entity : public Node {
+public : 
+  Entity();
+  Entity(Sprite* sprite);
+
+  Sprite* GetSprite();
+  void BindSprite(Sprite* sprite);
+
+  ~Entity();
+
+private:
+  Sprite* sprite;
+protected:
+  INT32 attack = 3;
+  INT32 armor = 5;
+  INT32 HP = 5;
+  INT32 MP = 200;
+};
+
+#endif;
