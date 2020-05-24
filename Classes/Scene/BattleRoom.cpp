dac18318d1bf678729383ac21d1e09612a3810ec
Shaun-Shane <1953181@tongji.edu.cn>
2020-05-24 21:32:50 +0800

    Add BattleRoom class

---
+++ Classes/Scene/BattleRoom.cpp
@@ -0,0 +1,54 @@
+#include "BattleRoom.h"
+
+bool BattleRoom::init() {
+  centerX = .0f, centerY = .0f;
+  x = 0, y = 0;
+  visTime = 0, nextDIR = 0; //Ä¬ÈÏÏòÓÒ
+  nextRoom = nullptr, prevRoom = nullptr;
+  
+  memset(visDir, 0, sizeof(visDir));
+  
+  enemyCtr = EnemyController::create(); //controll enemy behaviors
+  return true; 
+}
+
+void BattleRoom::setCenter(float X, float Y) { centerX = X, centerY = Y; }
+
+float BattleRoom::getCenterX() { return centerX; }
+
+float BattleRoom::getCenterY() { return centerY; }
+
+INT32 BattleRoom::getX() { return x; }
+
+INT32 BattleRoom::getY() { return y; }
+
+void BattleRoom::createMap() {
+  const float X = centerX - FLOORWIDTH * (SIZEROOM / 2);
+  const float Y = centerY + FLOORWIDTH * (SIZEROOM / 2);
+
+  float curX = X, curY = Y;
+  for (INT32 y = SIZEROOM - 1; y >= 0; y--) {
+    for (INT32 x = 0; x < SIZEROOM; x++) {
+      Sprite* tmpSprite = nullptr;
+      if (y == 0 || y == SIZEROOM - 1 || x == 0 || x == SIZEROOM - 1) {
+        tmpSprite = Sprite::create("Map//wall1.png");
+        this->addChild(tmpSprite, 1);
+      }
+      else {
+        tmpSprite = Sprite::create("Map//floor1.png");
+        this->addChild(tmpSprite, 0);
+      }
+
+      //assert(tmpSprite == nullptr);
+      tmpSprite->setPosition(Point(curX, curY));
+      
+      vecBG.pushBack(tmpSprite);
+      curX += FLOORWIDTH;
+    }
+    curX = X, curY -= FLOORHEIGHT;
+  }  
+}
+
+void connect(BattleRoom* other) {
+
+}
---
+++ Classes/Scene/BattleRoom.h
@@ -0,0 +1,45 @@
+#ifndef _BATTLEROOM_H
+#define _BATTLEROOM_H
+
+#include "cocos2d.h"
+#include "Enemy\EnemyController.h"
+#include "Const\Const.h"
+USING_NS_CC;
+
+class BattleRoom : public Node {
+  static constexpr INT32 SIZEROOM = 19;
+  friend class BattleScene;
+ public:
+  CREATE_FUNC(BattleRoom);
+
+  virtual bool init();
+  //virtual void update(float delta);
+  float getCenterX();
+  float getCenterY();
+  INT32 getX();
+  INT32 getY();
+
+  void createMap(); //生成该房间地图
+
+  void setCenter(float, float); // 设置中心
+
+  void connect(BattleRoom*);
+
+ private:
+  float centerX, centerY;
+  INT32 x, y;  // row and column in 7 * 7 BattleRoom matrix
+  INT32 visTime; //maybe not needed
+  INT32 nextDIR;
+  bool visDir[CNTDIR] = {false};
+
+ private:
+  BattleRoom* nextRoom = nullptr;
+  BattleRoom* prevRoom = nullptr;
+
+  EnemyController* enemyCtr = nullptr;
+
+ private:
+  Vector<Sprite*> vecBG;
+};
+
+#endif
