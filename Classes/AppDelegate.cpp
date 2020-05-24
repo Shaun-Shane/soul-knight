f08f5b5db19413410a5721896a2930f9da1e735c
Shaun-Shane <1953181@tongji.edu.cn>
2020-05-24 21:31:16 +0800

    Update AppDelegate.cpp

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -25,6 +25,7 @@
 #include "AppDelegate.h"
 #include "Scene\HelloWorldScene.h"
 #include "Scene\SafeScene.h"
+#include "Scene\BattleScene.h"
 
 // #define USE_AUDIO_ENGINE 1
 // #define USE_SIMPLE_AUDIO_ENGINE 1
@@ -119,7 +120,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     register_all_packages();
 
     // create a scene. it's an autorelease object
-    auto scene = SafeScene::createScene();
+    auto scene = BattleScene::createScene();
 
     // run
     director->runWithScene(scene);
