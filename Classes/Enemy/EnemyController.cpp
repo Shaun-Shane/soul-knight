40e4feff4be6a5543535fc10829afdd33dfeed15
Shaun-Shane <1953181@tongji.edu.cn>
2020-05-24 21:32:39 +0800

    Add EnemyController class

---
+++ Classes/Enemy/EnemyController.cpp
@@ -0,0 +1,45 @@
+#include "EnemyController.h"
+#include "Enemy.h"
+#include "Actor\Knight.h"
+
+bool EnemyController::init() {
+  createEnemy();
+  this->scheduleUpdate();
+  return true;
+}
+
+void EnemyController::createEnemy() {
+  Enemy* enemy = nullptr;
+  Sprite* sprite = nullptr;
+
+  srand(static_cast<unsigned int>(time(nullptr)));
+  int maxI = static_cast<int>(CCRANDOM_0_1() * (MAX_ENEMY_NUM - MIN_ENEMY_NUM)) + MIN_ENEMY_NUM;
+  
+  for (int i = 0; i < maxI; i++) {
+    enemy = Enemy::create();
+    enemy->bindSprite(Sprite::create("Enemy//shooter.png"));
+    enemy->reset();
+
+    this->addChild(enemy);
+    vecEnemy.pushBack(enemy);
+  }
+}
+
+void EnemyController::update(float delta) {
+  Size visibleSize = Director::getInstance()->getVisibleSize();
+  srand(static_cast<unsigned int>(time(nullptr)));
+  for (auto enemy : vecEnemy) {
+    if (enemy->isAlive()) {
+      enemy->setPosition(visibleSize.width * CCRANDOM_0_1(), visibleSize.height * CCRANDOM_0_1());
+
+      if (enemy->getHP() <= 0) {
+        enemy->hide();
+      } else {
+        enemy->show();
+      }
+
+    }
+  }
+}
+
+void EnemyController::bindKnight(Knight* knight) { this->knight = knight; }
