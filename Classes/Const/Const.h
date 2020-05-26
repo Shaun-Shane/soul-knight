35dc9f63a0786af18893750b1c82cf596a0bea09
Shaun-Shane <1953181@tongji.edu.cn>
2020-05-26 10:45:15 +0800

    Update Const.h

--- Classes/Const/Const.h
+++ Classes/Const/Const.h
@@ -4,6 +4,7 @@
 
 constexpr INT32 CNTDIR = 4;
 
+enum {RIGHT, UP, LEFT, DOWN};
 constexpr INT32 DIRX[CNTDIR] = {1, 0, -1, 0};  // right up left down
 constexpr INT32 DIRY[CNTDIR] = {0, 1, 0, -1};  // right up left down
 
@@ -13,7 +14,7 @@ constexpr INT32 FLOORHEIGHT = 40;
 constexpr INT32 WALLWIDTH = 40;
 constexpr INT32 WALLHEIGHT = 60;
 
-constexpr INT32 HALLWAYCNTW = 5;
+constexpr INT32 SIZEHALL = 7;
 
 constexpr INT32 CENTERDIS = 39 * 40;
 #endif  // !_CONST_H_
