27590c0b7b9da4cefe2cd2ad6138b2b4b27143b0
Shaun-Shane <1953181@tongji.edu.cn>
2020-06-04 12:38:06 +0800

    Update Knight.cpp
    增加人物秘技 修改监听回调函数

--- Classes/Actor/Knight.cpp
+++ Classes/Actor/Knight.cpp
@@ -48,10 +48,29 @@ void Knight::registerKeyboardEvent() {
         useUltimateSkill();
         break;
     }
+
+    if (abs(moveSpeedX) > 0 && abs(moveSpeedY) > 0) //确保任意方向速度相同
+      moveSpeedX /= sqrt(2.0f), moveSpeedY /= sqrt(2.0f);  
   };
 
   listener->onKeyReleased = [&](EventKeyboard::KeyCode code, Event*) {
-    moveSpeedX = .0f, moveSpeedY = .0f;
+    switch (code) {
+      case EventKeyboard::KeyCode::KEY_D:
+        moveSpeedX = .0f;
+        break;
+
+      case EventKeyboard::KeyCode::KEY_W:
+        moveSpeedY = .0f;
+        break;
+
+      case EventKeyboard::KeyCode::KEY_A:
+        moveSpeedX = .0f;
+        break;
+
+      case EventKeyboard::KeyCode::KEY_S:
+        moveSpeedY = .0f;
+        break;
+    }
   };
 
   _eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
@@ -64,16 +83,49 @@ void Knight::weaponAttack() {
 void Knight::useUltimateSkill() {
   if (haveUltimateSkill) {
     log("using ultimate skill!");
+
+    auto skillCircle = DrawNode::create();
+    skillCircle->drawSolidCircle(getPosition(), 220.0f,
+                                 CC_DEGREES_TO_RADIANS(360), 100,
+                                 Color4F(1.0f, 0.8f, .0f, 0.3f));
+    skillCircle->setGlobalZOrder(LayerPlayer);
+
+    auto fadeIn = FadeIn::create(0.2f); 
+    auto fadeOut = FadeOut::create(0.3f);
+    auto sequence = Sequence::create(fadeIn, fadeOut,
+                                     RemoveSelf::create(true), NULL);
+
     if (this->atBattleRoom == nullptr) {
-      //haveUltimateSkill = false;
+      assert(atHall != nullptr);
+      atHall->addChild(skillCircle);
+
+      skillCircle->runAction(sequence);
       return;
     }
+
+    atBattleRoom->addChild(skillCircle);
+
+    skillCircle->runAction(sequence);
+
     Vector<Enemy*>& vecEnemy = atBattleRoom->getVecEnemy();
 
-    for (auto e : vecEnemy) e->removeFromParent();
+    for (auto& e : vecEnemy) {
+      if (e == nullptr) continue;
 
-    vecEnemy.clear();
-    //
+      float enemyX = e->getPositionX(), enemyY = e->getPositionY();
+
+      if (sqrt(pow(getPositionX() - enemyX, 2) +
+               pow(getPositionY() - enemyY, 2)) <= 220.0f)
+        e->removeFromParent(); //秒杀怪物 从父类移除
+    }
+
+    bool allKilled = true; //判断是否全被击杀
+    for (auto e : vecEnemy) {
+      if (e->getParent() != nullptr) allKilled = false;
+    }
+
+    if (allKilled) vecEnemy.clear();
+
   }
 }
 
