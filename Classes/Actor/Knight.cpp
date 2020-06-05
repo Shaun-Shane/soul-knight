903dbe7682d55a78c6ce7ddcbc753596158236ac
Shaun-Shane <1953181@tongji.edu.cn>
2020-06-05 11:34:14 +0800

    Update

--- Classes/Actor/Knight.cpp
+++ Classes/Actor/Knight.cpp
@@ -54,14 +54,6 @@ void Knight::registerKeyboardEvent() {
       case EventKeyboard::KeyCode::KEY_K:
         useUltimateSkill();
         break;
-
-      case EventKeyboard::KeyCode::KEY_J:
-        weaponAttack(); 
-        break;
-
-      case EventKeyboard::KeyCode::KEY_K:
-        useUltimateSkill();
-        break;
     }
 
     if (abs(moveSpeedX) > 0 && abs(moveSpeedY) > 0) //确保任意方向速度相同
