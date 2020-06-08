fba80287949a8593af333ec4e5efbad317208190
Shaun-Shane <1953181@tongji.edu.cn>
2020-06-08 22:05:19 +0800

    Update Enemy
    加入敌人AI(代修改)

--- Classes/Enemy/Enemy.cpp
+++ Classes/Enemy/Enemy.cpp
@@ -48,3 +48,85 @@ bool Enemy::isCollideWithKnight(Knight * knight) {
 void Enemy::makeCoinside() {
     this->getSprite()->setPosition(this->getPosition());
 }//将精灵位置跟enemy类Node位置调统一
+
+void Enemy::patrolRoute(const BattleRoom* battleRoom, Knight* knight) {
+  const Point enemyPos = this->getPosition();
+
+  if (paceCount % 40) {
+    this->setPosition(
+        Point(enemyPos.x + DIRX[wayOfPace] - knight->getMoveSpeedX(),
+              enemyPos.y + DIRY[wayOfPace] - knight->getMoveSpeedY()));
+    this->makeCoinside();
+    paceCount++;
+    return;
+  }
+
+  paceCount = 1;  //设置为1，避免下一轮被paceCount%40识别为false
+  wayCanBeSelected.clear();
+  for (INT32 dir = 0; dir < CNTDIR; dir++) {
+    if (true) { //4各可以走的方向
+      wayCanBeSelected.push_back(dir);
+    }
+  }  //选取可以走的方向
+
+  srand(static_cast<unsigned int>(time(nullptr)));
+  wayOfPace = wayCanBeSelected[rand() % wayCanBeSelected.size()];
+  this->setPosition(
+      Point(enemyPos.x + DIRX[wayOfPace] - knight->getMoveSpeedX(),
+            enemyPos.y + DIRY[wayOfPace] - knight->getMoveSpeedY()));
+  this->makeCoinside();
+
+}  //在没探测到骑士的时候正常的巡逻路线
+
+void Enemy::aiOfEnemy(Knight* knight, BattleRoom* battleRoom) {
+  if (knight == nullptr || battleRoom == nullptr) {
+    return;
+  }
+
+  const Point enemyPos = this->getPosition();
+  const Point knightPos = knight->getPosition();
+  const INT32 disBetweenEnemyAndKnight =
+      enemyPos.getDistance(knightPos);  //获取二者距离，用于后续判断
+
+  if (disBetweenEnemyAndKnight > SIGHTRANGE) {
+    patrolRoute(battleRoom, knight);
+  } else {
+    paceCount = 0;
+    wayOfPace = -1;
+    if (disBetweenEnemyAndKnight > ATTACKRANGE) {
+      this->setPosition(
+          Point(enemyPos.x +
+                    2 * (knightPos.x - enemyPos.x) / disBetweenEnemyAndKnight -
+                    knight->getMoveSpeedX(),
+                enemyPos.y +
+                    2 * (knightPos.y - enemyPos.y) / disBetweenEnemyAndKnight -
+                    knight->getMoveSpeedY()));
+      this->makeCoinside();
+    } else {
+      attackTheKnight(knight, disBetweenEnemyAndKnight);
+    }
+  }
+}
+
+void Enemy::attackTheKnight(Knight* knight,
+                                INT32 disBetweenEnemyAndKnight) {
+  if (disBetweenEnemyAndKnight <= 5) {
+    if (attackTimeCount % 40 == 0) {
+      knight->deductHP(3);
+      attackTimeCount = 1;
+    }
+    attackTimeCount++;
+    return;
+  }
+
+  else {
+    const Point enemyPos = this->getPosition();
+    const Point knightPos = knight->getPosition();
+    this->setPosition(Point(
+        enemyPos.x + 2 * (knightPos.x - enemyPos.x) / disBetweenEnemyAndKnight -
+            knight->getMoveSpeedX(),
+        enemyPos.y + 2 * (knightPos.y - enemyPos.y) / disBetweenEnemyAndKnight -
+            knight->getMoveSpeedY()));
+    this->makeCoinside();
+  }  //等武器那一块出来加上武器
+}
