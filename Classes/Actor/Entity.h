3b23fccaad4e2ea81e498b728f61475330ec3efe
Shaun-Shane <1953181@tongji.edu.cn>
2020-06-12 11:00:10 +0800

    Update Entity.h
    将deductHp函数改为virtual

--- Classes/Actor/Entity.h
+++ Classes/Actor/Entity.h
@@ -15,8 +15,9 @@ public :
   void addShadow(const Point&, INT32);  //添加影子
 
   INT32 getHP(); // return HP of this entity
-  void deductHP(INT32); //扣血
+  virtual void deductHP(INT32); //扣血
   void setHP(INT32 HP);
+
   float getMoveSpeedX();
   float getMoveSpeedY();
 
