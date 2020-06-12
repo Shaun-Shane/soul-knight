2df024a7bd4704bfd7cdda13c9ba26888bf1a402
Caviar011230 <1458167787@qq.com>
2020-06-12 16:08:33 +0800

    设置了两种敌人，一种近战，一种远战，利用不同标签数区分以及调用彼此不同的攻击函数

--- Classes/Enemy/Enemy.cpp
+++ Classes/Enemy/Enemy.cpp
@@ -46,6 +46,34 @@ bool Enemy::isCollideWithKnight(Knight * knight) {
   return entityRec.containsPoint(enemyPos);
 }
 
+void Enemy::setType(int type){
+    enemyType = type;
+    boarRushCount = 0;
+    setAttackRange();
+}
+
+void Enemy::setAttackRange(){
+    switch (enemyType)
+    {
+    case 0:
+        ATTACKRANGE = 200;//弓箭手
+    case 1:
+        ATTACKRANGE = SIGHTRANGE;//野猪
+    default://暂时还没想出别的
+        break;
+    }
+}
+
+bool Enemy::inRoom(const BattleRoom* battleRoom,Point myPos){
+	Point upLeftPos = battleRoom->getUpleftVertex();
+	Point downRightPos = battleRoom->getDownRightVertex();
+    if (myPos.x>=upLeftPos.x&&myPos.x<=downRightPos.x&&
+        myPos.y>=downRightPos.y&&myPos.y<=upLeftPos.y) {
+        return true;
+    }
+    return false;
+}
+
 void Enemy::patrolRoute(const BattleRoom* battleRoom, Knight* knight) {
   const Point enemyPos = this->getPosition();
 
@@ -112,28 +140,96 @@ void Enemy::aiOfEnemy(Knight* knight, const BattleRoom* battleRoom) {
                                                disBetweenEnemyAndKnight+ 3.0f * shiftSeed));
       followCount++;
     } else {
-      attackTheKnight(knight, disBetweenEnemyAndKnight);
+      attackTheKnight(knight, disBetweenEnemyAndKnight,battleRoom);
     }
   }
 }
 
 void Enemy::attackTheKnight(Knight* knight,
-                                float disBetweenEnemyAndKnight) {
-  if (disBetweenEnemyAndKnight < ATTACKRANGE) {
-    if (attackTimeCount % 40 == 0) {
-      knight->deductHP(3);
-      log("%d", knight->getHP());
-      attackTimeCount = 1;
+    float disBetweenEnemyAndKnight,const BattleRoom* battleRoom) {
+    if (disBetweenEnemyAndKnight < ATTACKRANGE) {
+        switch (enemyType)
+        {
+        case 0:
+            archerAttack(knight, disBetweenEnemyAndKnight);
+            break;
+        case 1:
+            boarAttack(knight, disBetweenEnemyAndKnight,battleRoom);
+            break;
+        default:
+            break;
+        }
     }
-    attackTimeCount++;
-    return;
-  }
+}
 
-  else {
-    const Point enemyPos = this->getPosition();
-    const Point knightPos = knight->getPosition();
-    //this->setPosition(Point(  
-    //    enemyPos.x + 3.0f * (knightPos.x - enemyPos.x) / disBetweenEnemyAndKnight,
-    //    enemyPos.y + 3.0f * (knightPos.y - enemyPos.y) / disBetweenEnemyAndKnight));
-  }  //等武器那一块出来加上武器
+/*弓箭手属于典型远攻类型，所以到达ATTACKRANGE之后不会再移动，而是直接射击*/
+void Enemy::archerAttack(Knight* knight, float disBetweenEnemyAndKnight){
+	auto enemyPos = this->getPosition();
+	auto knightPos = knight->getPosition();
+
+    if (attackTimeCount >= 80) {
+        knight->deductHP(3);
+        log("%d", knight->getHP());
+        attackTimeCount = 0;
+    }
+    else {
+        attackTimeCount++;
+    }
+}
+
+/*野猪属于冲撞攻击类，所以在比较靠近骑士之后会开启加速冲撞攻击*/
+void Enemy::boarAttack(Knight* knight, float disBetweenEnemyAndKnight, const BattleRoom* battleRoom) {
+	auto enemyPos = this->getPosition();
+    auto knightPos = knight->getPosition();
+
+    if (boarHaveAttacked) {
+        if (attackTimeCount++ >= 60) {
+            boarHaveAttacked = false;
+            attackTimeCount = 0;
+            return;
+        }
+        if (inRoom(battleRoom,Point(enemyPos.x + 5.0f * boarBumpDirection[0] + 0.5f * shiftSeed,
+            enemyPos.y + 5.0f * boarBumpDirection[1] + 0.5f * shiftSeed))) {
+            this->setPosition(Point(enemyPos.x + 5.0f * boarBumpDirection[0] + 0.5f * shiftSeed,
+                enemyPos.y + 5.0f * boarBumpDirection[1] + 0.5f * shiftSeed));
+        }
+        return;
+    }
+
+    if (!boarRest&&boarRushCount < 90) {
+        if (disBetweenEnemyAndKnight >= 50) {
+            this->setPosition(Point(enemyPos.x + 6.0f * (knightPos.x - enemyPos.x) /
+                disBetweenEnemyAndKnight,
+                enemyPos.y + 6.0f * (knightPos.y - enemyPos.y) /
+                disBetweenEnemyAndKnight));
+            boarBumpDirection[0] = (knightPos.x - enemyPos.x) / disBetweenEnemyAndKnight;
+            boarBumpDirection[1] = (knightPos.y - enemyPos.y) / disBetweenEnemyAndKnight;//保存最近的方向，给之后冲走使用
+        }
+        else{
+            this->setPosition(Point(enemyPos.x + 8.0f * boarBumpDirection[0],
+                enemyPos.y + 8.0f * boarBumpDirection[1]));
+        }
+        boarRushCount++;
+    }
+    else if (!boarRest&&boarRushCount >= 90) {
+        boarRest = true;
+    }
+    else if (boarRushCount <= 0) {
+        boarRest = false;
+    }
+    else {
+        boarRushCount--;
+    }
+
+    if (attackTimeCount >= 80 && disBetweenEnemyAndKnight < 10) {
+        boarBumpDirection[0] = (knightPos.x - enemyPos.x) / disBetweenEnemyAndKnight;
+        boarBumpDirection[1] = (knightPos.y - enemyPos.y) / disBetweenEnemyAndKnight;//保存最近的方向，给之后冲走使用
+        knight->deductHP(2);
+        log("%d", knight->getHP());
+        attackTimeCount = 0;
+        boarHaveAttacked = true;
+    }
+    else {
+        attackTimeCount++;
+    }  
 }
