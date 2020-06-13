4302cfc612e5ccb0b0e628922b96fa516dfc4635
Caviar011230 <1458167787@qq.com>
2020-06-13 15:00:22 +0800

    增加了已有敌人的转向效果
    现在敌人行走方向与图片上头的朝向相符了

--- Classes/Enemy/Enemy.h
+++ Classes/Enemy/Enemy.h
@@ -34,6 +34,8 @@ class Enemy : public Entity {
   void attackTheKnight(Knight* knight, float disBetweenEnemyAndKnight, const BattleRoom* battleRoom);
   void setAttackRange();
   bool inRoom(const BattleRoom* battleRoom,Point myPos);
+  void spriteChangeDirection();
+  void recordX(float x);
 
 private:
     void archerAttack(Knight* knight, float disBetweenEnemyAndKnight);//当敌人设定类型为弓箭手时调用的攻击函数
@@ -54,6 +56,10 @@ private:
   std::vector<INT32> wayCanBeSelected;  //可供选择的行走方向
   float shiftSeed=0.0f;
   INT32 followCount = 0;
+
+  bool xWayJustChanged = false;//判断x方向速度的正负是否刚改变过
+  bool xIsPositive = true;//判断x方向正负，与上面一行变量配套使用
+  bool lastXIsPositive = true;//更前面一步x正负
 };
 
 #endif
