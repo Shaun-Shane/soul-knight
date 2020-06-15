216dffaa53a5c1022123c183c04c3ed105223dbe
YangXin <1931801344@qq.com>
2020-06-15 13:38:32 +0800

    添加受伤显示扣血的特效

---
+++ Classes/Actor/FlowWord.h
@@ -0,0 +1,18 @@
+﻿#ifndef _FlowWord_H_
+#define _FlowWord_H_
+
+#include "cocos2d.h"
+USING_NS_CC;
+
+class FlowWord : public Node {
+public:
+	CREATE_FUNC(FlowWord);
+	virtual bool init();
+
+public:
+	void showWord(int num, Point pos);
+private:
+	Label* m_textLab;
+};
+
+#endif
